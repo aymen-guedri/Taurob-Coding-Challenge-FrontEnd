e06dcfcc12b096bbce2bc481a50905611b11fc34
Aymen Guedri <aymenguedri01@gmail.com>
2024-08-10 14:12:39 +0100

    add mission form

--- src/components/MissionForm.js
+++ src/components/MissionForm.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './MissionForm.css';
 
-const MissionForm = ({ mission = {}, onSubmit }) => {
-  const [name, setName] = useState(mission.name || '');
-  const [description, setDescription] = useState(mission.description || '');
-  const [robotId, setRobotId] = useState(mission.robot?.id || '');
-
+const MissionForm = ({ onSubmit }) => {
+  const [missions, setMissions] = useState([]);
+  const [selectedMissionId, setSelectedMissionId] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [robotId, setRobotId] = useState('');
   const [robots, setRobots] = useState([]);
+  const [message, setMessage] = useState('');
+  const [messageColor, setMessageColor] = useState('');
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/missions/`)
+      .then(response => setMissions(response.data.missions))
+      .catch(error => console.error(error));
+  }, []);
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_API_URL}/robots/`)
@@ -14,33 +24,91 @@ const MissionForm = ({ mission = {}, onSubmit }) => {
       .catch(error => console.error(error));
   }, []);
 
+  useEffect(() => {
+    if (selectedMissionId) {
+      const mission = missions.find(m => m.id === selectedMissionId);
+      if (mission) {
+        setName(mission.name);
+        setDescription(mission.description);
+        setRobotId(mission.robot.id);
+      }
+    } else {
+      setName('');
+      setDescription('');
+      setRobotId('');
+    }
+  }, [selectedMissionId, missions]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const missionData = { name, description, robot: robotId };
-
-    if (mission.id) {
-      axios.put(`${process.env.REACT_APP_API_URL}/mission/${mission.id}/`, missionData)
-        .then(response => onSubmit(response.data.mission))
-        .catch(error => console.error(error));
-    } else {
-      axios.post(`${process.env.REACT_APP_API_URL}/mission/`, missionData)
-        .then(response => onSubmit(response.data.mission))
-        .catch(error => console.error(error));
-    }
+  
+    const request = selectedMissionId
+      ? axios.put(`${process.env.REACT_APP_API_URL}/missions/${selectedMissionId}/`, missionData)
+      : axios.post(`${process.env.REACT_APP_API_URL}/missions/`, missionData);
+  
+    request
+      .then(response => {
+        onSubmit(response.data.mission);
+        setMessage(selectedMissionId ? 'Mission updated successfully!' : 'Mission created successfully!');
+        setMessageColor('green');
+        setTimeout(() => setMessage(''), 5000);
+        setName('');
+        setDescription('');
+        setRobotId('');
+        setSelectedMissionId('');
+      })
+      .catch(error => {
+        if (error.response) {
+          console.error('Error response:', error.response);
+          if (error.response.data) {
+            console.error('Error details:', error.response.data);
+          }
+        } else {
+          console.error('Error message:', error.message);
+        }
+        setMessage('Failed to submit mission.');
+        setMessageColor('red');
+        setTimeout(() => setMessage(''), 5000);
+      });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" required />
-      <textarea value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" required />
-      <select value={robotId} onChange={(e) => setRobotId(e.target.value)} required>
-        <option value="">Select Robot</option>
-        {robots.map(robot => (
-          <option key={robot.id} value={robot.id}>{robot.name}</option>
-        ))}
-      </select>
-      <button type="submit">Submit</button>
-    </form>
+    <div className="mission-form-container">
+      <h1>{selectedMissionId ? 'Update Mission' : 'Create Mission'}</h1>
+      <p style={{color:"blue"}}>{selectedMissionId ? 'Update existing mission' : 'Create new mission'}</p>
+      <div className="mission-select-container">
+        <label htmlFor="mission-select"></label>
+        <select id="mission-select" onChange={(e) => setSelectedMissionId(e.target.value)} value={selectedMissionId}>
+          <option value="">Create new Mission</option>
+          {missions.map(mission => (
+            <option key={mission.id} value={mission.id}>{mission.name}</option>
+          ))}
+        </select>
+      </div>
+      <form onSubmit={handleSubmit} className="mission-form">
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+          required={!selectedMissionId}
+        />
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Description"
+          required={!selectedMissionId}
+        />
+        <select value={robotId} onChange={(e) => setRobotId(e.target.value)} required={!selectedMissionId}>
+          <option value="">Select Robot</option>
+          {robots.map(robot => (
+            <option key={robot.id} value={robot.id}>{robot.name}</option>
+          ))}
+        </select>
+        <button type="submit">{selectedMissionId ? 'Update Mission' : 'Create Mission'}</button>
+      </form>
+      {message && <p style={{ color: messageColor }}>{message}</p>}
+    </div>
   );
 };
 
